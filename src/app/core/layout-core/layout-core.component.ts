b5a637056b3876ed18bc80e5cd6cec5c484cc109
Bruno Cunha <rocks.bn@gmail.com>
2018-07-27 20:04:45 -0300

    inicial

---
+++ src/app/core/layout-core/layout-core.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-layout-core',
+  templateUrl: './layout-core.component.html',
+  styleUrls: ['./layout-core.component.css'],
+})
+export class LayoutCoreComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+    
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  
+  }
